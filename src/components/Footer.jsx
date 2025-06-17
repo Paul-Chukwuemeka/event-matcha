c89c7c3be64b3e66c60ab847de2cf062842b56d6
paul-chukwuemeka <paulchukwuemeka123@gmail.com>
2025-06-17 16:55:28 +0100

    Add footer and header components, integrate Tailwind CSS, and set up Vite configuration
    - Implemented Footer component with logo, quick links, subscription form, and social media icons.
    - Created Header component with logo and navigation links.
    - Added Tailwind CSS for styling in index.css.
    - Configured Vite with Tailwind CSS and React plugins in vite.config.js.
    - Established main entry point in main.jsx to render the App component.
    - Added logo and chat bubble assets for the footer.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,75 @@
+import logo from "../assets/Footer-Logo.png";
+import chatBubble from "../assets/Chat burble.png";
+import { BsTwitter, BsFacebook, BsInstagram } from "react-icons/bs";
+import { FaAngleDown } from "react-icons/fa6";
+import { PiTiktokLogoFill } from "react-icons/pi";
+import { IoIosChatboxes } from "react-icons/io";
+
+const Footer = () => {
+  return (
+    <div className="bg-[#005F61] py-10 px-20">
+      <section className="flex justify-between p-6 gap-50">
+        <div className="flex flex-col gap-4 flex-1 w-fit">
+          <img src={logo} alt="logo" className="w-60" />
+          <p className="text-white text-lg font-medium max-w-[430px]">
+            Connect, book, and showcase event services with ease and
+            sophistication.
+          </p>
+        </div>
+        <ul className="text-white flex flex-col gap-3 text-lg">
+          <li className="font-semibold">Quick Links</li>
+          <li>Home</li>
+          <li className="flex items-center gap-1">
+            Our Services <FaAngleDown />
+          </li>
+          <li>About Us</li>
+          <li>Blog</li>
+          <li>Releases</li>
+        </ul>
+        <div className="text-white flex flex-col gap-4">
+          <p className="text-lg font-semibold">Stay up to date</p>
+          <p className="text-lg w-[450px]">
+            Be the first to know when we release new features, make improvements
+            or when there is an offer.No spam
+          </p>
+          <div className="flex gap-5">
+            <input
+              type="text"
+              className="bg-white text-lg p-2.5 placeholder:text-[#757575] text-black font-medium rounded-md"
+              placeholder="Enter your email"
+            />
+            <button className="bg-[#F78105] w-28 rounded-lg">Subscribe</button>
+          </div>
+          <div className="flex mt-8 text-3xl gap-5 cursor-pointer">
+            <BsTwitter />
+            <BsFacebook />
+            <BsInstagram />
+            <PiTiktokLogoFill />
+          </div>
+        </div>
+      </section>
+      <div>
+        <div className="border-[#008C8F] border h-0.5 flex items-center justify-end mt-6 cursor-pointer">
+          <img src={chatBubble} alt="Chat Bubble" className="w-54" />
+          <button className="bg-[#005F61] text-white shadow-[0_0_5px] shadow-[#00000029] text-xl p-4 rounded-full ">
+            <IoIosChatboxes />
+          </button>
+        </div>
+      </div>
+      <div className="p-2 mt-8 flex justify-between">
+        <p className="text-[#D1D1D1]">
+          © 2024 D’EventMatcha. All rights reserved.
+        </p>
+        <ul className="flex items-center gap-4 font-semibold text-[#D1D1D1]">
+          <li>Terms</li>
+          <div className="h-1.5 w-1.5 rounded-full bg-[#D1D1D1] "></div>
+          <li>Privacy</li>
+          <div className="h-1.5 w-1.5 rounded-full bg-[#D1D1D1] "></div>
+          <li>Cookies</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import logo from "../assets/Logo.png";
+import { BiSearch } from "react-icons/bi";
+import { FaRegUser, FaAngleDown } from "react-icons/fa6";
+
+const Header = () => {
+  return (
+    <div className="flex items-center justify-evenly p-4 shadow-[10px_10px_10px] shadow-[#f7820509]">
+      <img src={logo} alt="logo" className="w-54" />
+      {/* <div className="flex border items-center p-2  bg-[#F4F4F4] border-[#D1D1D1] w-[600px] rounded-lg gap-1">
+        <BiSearch className="text-xl text-[#A3A3A3] "/>
+        <input type="text" placeholder="Search anything..." className="flex-1" />
+      </div> */}
+
+      <ul className="flex w-1/4 text-lg justify-between *:hover:text-[#005F61] *:cursor-pointer ">
+        <li>Home</li>
+        <li>About</li>
+        <li className="flex items-center gap-1">Services <FaAngleDown/></li>
+        <li>Contact Us</li>
+        <li>Blog</li>
+      </ul>
+      {/* <div className="flex gap-2">
+        <button className="p-2 cursor-pointer bg-[#F4F4F4] rounded-full">
+          <FaRegUser className="text-lg" />
+        </button>
+        <button>
+          <FaAngleDown />
+        </button>
+      </div> */}
+      <div className="gap-10 flex">
+        <button className="text-[#005F61] cursor-pointer  text-lg font-semibold">Login</button>
+        <button className="bg-[#005F61] cursor-pointer text-lg font-semibold p-2.5 rounded-lg text-white">Sign Up for Free</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;

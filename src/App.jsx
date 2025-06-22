8196fe8e0063f3be9450bb2c363f467ae2ce8258
paul-chukwuemeka <paulchukwuemeka123@gmail.com>
2025-06-23 00:55:22 +0100

    Update package.json scripts, enhance Landing page layout, and refine global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
+import '@fontsource/lato/100.css';
+import '@fontsource/lato/300.css';
+import '@fontsource/lato/400.css';
+import '@fontsource/lato/700.css';
+import '@fontsource/lato/900.css';
 
 const App = () => {
   return (
--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -9,18 +9,24 @@ const Landing = () => {
     <div className="grid grid-rows-[auto_1fr_auto] min-h-screen h-fit">
       <Header />
       <main className="w-full">
-        <section className="flex max-lg:flex-col p-4 ">
-          <div className="w-1/2 max-lg:w-full p-10 px-[8%]flex flex-col gap-5 items-start justify-start">
+        <section className="flex max-lg:flex-col p-10 ">
+          <div className="w-1/2 max-lg:w-full p-10 px-[8%] flex flex-col gap-5 items-start justify-start">
             <h1 className="text-[3em] font-semibold">
               Your Ultimate Destination for Booking and Showcasing{" "}
-              <span className="text-[#E67505]"><div className="inline relative "><span className="relative z-2">Premium</span> <img src={Vector} className="absolute z-0 top-0 -left-0" /> </div> Event Services</span>
+              <span className="text-[#E67505]">
+                <div className="inline relative ">
+                  <span className="relative z-2">Premium</span>{" "}
+                  <img src={Vector} className="absolute z-0 top-0 -left-0" />{" "}
+                </div>{" "}
+                Event Services
+              </span>
             </h1>
-            <p>
+            <p className="text-lg text-[#757575] max-w-[600px]">
               Our platform caters to both service providers showcasing their
               expertise and event needers seeking top-notch services. Connect,
               book, and showcase with ease and sophistication.
             </p>
-            <button>
+            <button className="flex items-center gap-2 bg-[#005F61] text-white font-extrabold px-6 py-3 rounded-lg shadow-[0_0_5px] shadow-[#00000029]">
               Get Started <FaArrowRightLong />
             </button>
           </div>

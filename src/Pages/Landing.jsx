12f97822d43cbaf5900dade6f16846635a20897d
paul-chukwuemeka <paulchukwuemeka123@gmail.com>
2025-06-24 13:10:17 +0100

    Add asset imports and exports in index.jsx for image resources

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,14 +1,95 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { FaArrowRightLong } from "react-icons/fa6";
-import HeroImg from "../assets/Frame-329.png";
-import Vector from "../assets/Vector 7.png";
-import PeopleImg from "../assets/people.png";
-import Person from "../assets/person.png";
-import Planner from "../assets/planner.png";
-import BookMark from "../assets/bookmark.png";
+import {
+  HeroImg,
+  Vector,
+  PeopleImg,
+  Person,
+  Planner,
+  BookMark,
+  ProductShot,
+  ReliableIcon,
+  NetworkIcon,
+  Network,
+  PersonalizedIcon,
+  Personalized,
+  FilterIcon,
+  Filter,
+  Featured1,
+  Featured2,
+  Featured3,
+  Featured4,
+  Featured5,
+  Featured6,
+  Featured7,
+  Featured8,
+  Featured9,
+} from "../assets/index";
 
 const Landing = () => {
+  const FeaturedImages = [
+    {
+      src: Featured1,
+      alt: "Featured 1",
+      new: false,
+      style: "object-fit max-lg:col-[1/2] max-lg:row-[1/2]",
+      tag: "weddings",
+    },
+    {
+      src: Featured2,
+      alt: "Featured 2",
+      new: false,
+      style: "object-fit max-lg:row-[1/3]",
+      tag: "weddings",
+    },
+    {
+      src: Featured3,
+      alt: "Featured 3",
+      new: false,
+      style: "object-fit max-lg:col-[1/2] max-lg:row-[4/7]",
+      tag: "venues",
+    },
+    {
+      src: Featured4,
+      alt: "Featured 4",
+      new: false,
+      style: "object-fit max-lg:col-[2/3] max-lg:row-[5/6]",
+      tag: "owambe",
+    },
+    {
+      src: Featured5,
+      alt: "Featured 5",
+      new: false,
+      style: "object-fit max-lg:col-[2/3] max-lg:row-[6/8]",
+      tag: "owambe",
+    },
+    {
+      src: Featured6,
+      alt: "Featured 6",
+      new: false,
+      style: "object-fit max-lg:col-[1/2] max-lg:row-[2/4]",
+    },
+    {
+      src: Featured7,
+      alt: "Featured 7",
+      new: false,
+      style: "object-fit max-lg:col-[2/3] max-lg:row-[3/5]",
+    },
+    {
+      src: Featured8,
+      alt: "Featured 8",
+      new: false,
+      style: "object-fit max-lg:col-[1/2] max-lg:row-[7/8]",
+    },
+    {
+      src: Featured9,
+      alt: "Featured 9",
+      new: false,
+      style: "object-fit max-lg:col-[1/3] max-lg:row-[8/9]",
+    },
+  ];
+
   return (
     <div className="grid grid-rows-[auto_1fr_auto] min-h-screen h-fit">
       <Header />
@@ -43,7 +124,7 @@ const Landing = () => {
           </div>
         </section>
         <section className="flex max-lg:flex-col bg-[#FFF8F0] p-25 max-lg:p-10 max-md:px-4 justify-center gap-4 my-30  ">
-          <div className=" lg:border-r-2 max-lg:border-b-2 border-[#D1D1D1] flex flex-1 justify-center max-lg:justify-start max-lg:p-2 items-center gap-6">
+          <div className=" lg:border-r-2 max-lg:border-b-2 border-[#D1D1D1] flex p-2 flex-1 justify-center max-lg:justify-start max-lg:p-2 items-center gap-6">
             <img src={PeopleImg} alt="People" className="w-16" />
             <div className="flex flex-col gap-4 max-lg:gap-2">
               <h2 className="font-semibold text-4xl max-lg:text-3xl  text-[#1E1E1E]">
@@ -54,7 +135,7 @@ const Landing = () => {
               </p>
             </div>
           </div>
-          <div className=" lg:border-r-2 max-lg:border-b-2 border-[#D1D1D1] flex flex-1 justify-center max-lg:justify-start max-lg:p-2 items-center gap-6">
+          <div className=" lg:border-r-2 max-lg:border-b-2 border-[#D1D1D1] flex p-2 flex-1 justify-center max-lg:justify-start max-lg:p-2 items-center gap-6">
             <img src={Person} alt="People" className="w-16" />
             <div className="flex flex-col gap-4 max-lg:gap-2">
               <h2 className="font-semibold text-4xl max-lg:text-3xl  text-[#1E1E1E]">
@@ -65,7 +146,7 @@ const Landing = () => {
               </p>
             </div>
           </div>
-          <div className=" lg:border-r-2 max-lg:border-b-2 border-[#D1D1D1] flex flex-1 justify-center max-lg:justify-start max-lg:p-2 items-center gap-6">
+          <div className=" lg:border-r-2 max-lg:border-b-2 border-[#D1D1D1] flex p-2 flex-1 justify-center max-lg:justify-start max-lg:p-2 items-center gap-6">
             <img src={Planner} alt="People" className="w-16" />
             <div className="flex flex-col gap-4 max-lg:gap-2">
               <h2 className="font-semibold text-4xl max-lg:text-3xl  text-[#1E1E1E]">
@@ -76,7 +157,7 @@ const Landing = () => {
               </p>
             </div>
           </div>
-          <div className=" border-[#D1D1D1] flex flex-1 justify-center max-lg:justify-start max-lg:p-2 items-center gap-6">
+          <div className=" border-[#D1D1D1] flex p-2 flex-1 justify-center max-lg:justify-start max-lg:p-2 items-center gap-6">
             <img src={BookMark} alt="People" className="w-16" />
             <div className="flex flex-col gap-4 max-lg:gap-2">
               <h2 className="font-semibold text-4xl max-lg:text-3xl  text-[#1E1E1E]">
@@ -88,10 +169,130 @@ const Landing = () => {
             </div>
           </div>
         </section>
-        <section>
-          <h2>Why D’EventMatcha?</h2>
-          <div>
-            
+        <section className="flex flex-col items-center justify-center  max-lg:gap-10 p-10 gap-10 max-md:px-4">
+          <h2 className="text-4xl font-semibold max-lg:text-3xl">
+            Why D’EventMatcha?
+          </h2>
+          <div className="flex max-lg:flex-col-reverse max-md:px-4 w-full items-center  ">
+            <div className="flex-1 max-lg:w-full">
+              <img
+                src={ProductShot}
+                alt="Product Shot"
+                className="w-full max-w-[700px]  rounded-xl"
+              />
+            </div>
+            <div className="flex w-1/2 flex-col gap-6 max-lg:gap-4  max-lg:mt-4 px-5 max-lg:w-full">
+              <img
+                src={ReliableIcon}
+                alt="Reliable Icon"
+                className="w-30 max-lg:w-20"
+              />
+              <h3 className="text-4xl font-semibold max-lg:text-4xl">
+                Reliability
+              </h3>
+              <p className="text-xl text-[#757575] max-w-[600px]">
+                We work only with reputable and vetted event service providers
+                to ensure reliability and quality for every event.
+              </p>
+            </div>
+          </div>
+          <div className="flex max-lg:flex-col-reverse flex-row-reverse  max-md:px-4 items-center w-full justify-between ">
+            <div className="w-1/2 max-lg:w-full flex items-center justify-center">
+              <img
+                src={Network}
+                alt="Product Shot"
+                className="w-full max-w-[700px] rounded-xl"
+              />
+            </div>
+            <div className="flex flex-col gap-6 max-lg:gap-4  max-lg:mt-4 w-1/2 px-4 max-lg:w-full">
+              <img
+                src={NetworkIcon}
+                alt="Reliable Icon"
+                className="w-30 max-lg:w-15"
+              />
+              <h3 className="text-4xl font-semibold max-lg:text-4xl">
+                Comprehensive Network
+              </h3>
+              <p className="text-xl text-[#757575] max-w-[600px]">
+                We have built an extensive network of trusted and experienced
+                event service providers, ensuring you have access to the best in
+                the industry.
+              </p>
+            </div>
+          </div>
+          <div className="flex-row flex max-lg:flex-col-reverse max-md:px-4  w-full items-center  ">
+            <div className="w-1/2 max-lg:w-full">
+              <img
+                src={Filter}
+                alt="Product Shot"
+                className="w-full max-w-[700px]  rounded-xl"
+              />
+            </div>
+            <div className="flex flex-col gap-6 max-lg:gap-4  max-lg:mt-4 w-1/2 px-4 max-lg:w-full">
+              <img
+                src={FilterIcon}
+                alt="Reliable Icon"
+                className="w-30 max-lg:w-20"
+              />
+              <h3 className="text-4xl font-semibold max-lg:text-4xl">
+                Efficiency and Convenience
+              </h3>
+              <p className="text-xl text-[#757575] max-w-[600px]">
+                Say goodbye to endless searching and inquiries. Our platform
+                streamlines the entire process, saving you time and effort in
+                finding the right event service providers for your event.
+              </p>
+            </div>
+          </div>
+          <div className="flex max-lg:flex-col-reverse flex-row-reverse max-md:px-4 w-full  items-center  ">
+            <div className="w-1/2 max-lg:w-full">
+              <img
+                src={Personalized}
+                alt="Product Shot"
+                className="w-full max-w-[700px]  rounded-xl"
+              />
+            </div>
+            <div className="flex flex-col gap-6 max-lg:gap-4  max-lg:mt-4 w-1/2 p-4 max-lg:w-full">
+              <div>
+                <img
+                  src={PersonalizedIcon}
+                  alt="Reliable Icon"
+                  className="w-30 max-lg:w-20"
+                />
+              </div>
+              <h3 className="text-4xl font-semibold max-lg:text-4xl">
+                Personalized Recommendations
+              </h3>
+              <p className="text-xl text-[#757575] max-w-[600px]">
+                Our algorithms learn from your preferences and requirements,
+                providing personalized recommendations that match your unique
+                event needs.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className="bg-[#F4FFFF] my-30 p-20 max-lg:p-4 flex flex-col items-center">
+          <h1 className="text-center text-4xl font-bold text-[#1E1E1E]">
+            Featured Images
+          </h1>
+          <p className=" mt-5 mb-10 text-lg">
+            See images of facilities and services from our users.
+          </p>
+          <div className="w-full overflow-hidden gap-2 max-w-[1400px] h-[600px] rounded-4xl grid grid-cols-[1fr_1fr_1fr_25%_15%] grid-rows-[1fr_30px_70px_1fr] max-lg:grid-cols-2 max-lg:grid-rows-[18%_6%_13%_3%_17%_5%_16%_19%] max-lg:gap-2 max-lg:h-[1600px] max-lg:w-full">
+            {FeaturedImages.map((image, index) => (
+              <div
+                key={index}
+                className={`overflow-hidden cursor-pointer ${
+                  image.new ? "border-4 border-[#E67505]" : ""
+                } ${image.style}`}
+              >
+                <img
+                  src={image.src}
+                  alt={image.alt}
+                  className="w-full hover:scale-110  h-full object-cover rounded-lg"
+                />
+              </div>
+            ))}
           </div>
         </section>
       </main>
---
+++ src/assets/index.jsx
@@ -0,0 +1,55 @@
+import HeroImg from "./HeroImg.png";
+import Vector from "./Vector 7.png";
+import PeopleImg from "./people.png";
+import Person from "./person.png";
+import Planner from "./planner.png";
+import BookMark from "./bookmark.png";
+import ProductShot from "./Product shot.png";
+import ReliableIcon from "./reliable-icon.png";
+import NetworkIcon from "./network-icon.png";
+import Network from "./network.png";
+import PersonalizedIcon from "./personalized-icon.png";
+import Personalized from "./personalized.png";
+import FilterIcon from "./filter-icon.png";
+import Filter from "./filter.png";
+import FooterLogo from "./Footer-Logo.png";
+import Logo from "./Logo.png";
+import ChatBurble from "./Chat burble.png";
+import Featured1 from "./featured1.png";
+import Featured2 from "./featured2.png";
+import Featured3 from "./featured3.png";
+import Featured4 from "./featured4.png";
+import Featured5 from "./featured5.png";
+import Featured6 from "./featured6.png";
+import Featured7 from "./featured7.png";
+import Featured8 from "./featured8.png";
+import Featured9 from "./featured9.png";
+
+export {
+  HeroImg,
+  Vector,
+  PeopleImg,
+  Person,
+  Planner,
+  BookMark,
+  ProductShot,
+  ReliableIcon,
+  NetworkIcon,
+  Network,
+  PersonalizedIcon,
+  Personalized,
+  FilterIcon,
+  Filter,
+  FooterLogo,
+  Logo,
+  ChatBurble,
+  Featured1,
+  Featured2,
+  Featured3,
+  Featured4,
+  Featured5,
+  Featured6,
+  Featured7,
+  Featured8,
+  Featured9,
+};
